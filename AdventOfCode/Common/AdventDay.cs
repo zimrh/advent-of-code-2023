36f0a57b7b597a61f5c920419214a0eb3f651e58
Robert Hyde <robert.hyde@citrix.com>
2023-12-05 08:56:14 +0000

    Maintenance: We had repeated data in the input files so created a basic check to see if there is a Part<n>.txt, if not just default to Actual/Sample.txt

--- AdventOfCode/Common/AdventDay.cs
+++ AdventOfCode/Common/AdventDay.cs
@@ -6,7 +6,14 @@ public abstract class AdventDay
 {
     public IEnumerable<string> ReadFromFile(TestType testType, Part part)
     {
-        using var file = File.OpenRead($"./inputs/{GetType().Name}/{testType}Part{part}.txt");
+        var fileName = $"./inputs/{GetType().Name}/{testType}Part{part}.txt";
+
+        if (!File.Exists(fileName))
+        {
+            fileName = $"./inputs/{GetType().Name}/{testType}.txt";
+        }
+        
+        using var file = File.OpenRead(fileName);
         using var sr = new StreamReader(file);
         
         while (!sr.EndOfStream)
