185d2c39db7cac640e7620b356753298b0574ffc
zimrh <zimrh@yahoo.co.uk>
2023-12-09 13:15:18 +0000

    Day9: Removed a whole load of repition

--- AdventOfCode/Day9.cs
+++ AdventOfCode/Day9.cs
@@ -10,70 +10,8 @@ public class Day9 : AdventDay
         var oasisHistory = GetOasisHistory(testType, part);
 
         return part == Part.One ?
-            RunPartOne(oasisHistory) :
-            RunPartTwo(oasisHistory);
-    }
-
-    public int RunPartTwo(List<List<List<int>>> oasisHistory)
-    {
-
-        for (int day = 0; day < oasisHistory.Count; day++)
-        {
-            var record = 0;
-            do
-            {
-                oasisHistory[day].Add(new List<int>());
-                for (int entry = 1; entry < oasisHistory[day][record].Count; entry++)
-                {
-                    oasisHistory[day][record + 1].Add(
-                        oasisHistory[day][record][entry] - oasisHistory[day][record][entry - 1]);
-                }
-                record++;
-            } while (!oasisHistory[day][record].All(r => r == 0));
-
-            oasisHistory[day][record].Insert(0, 0);
-
-            do
-            {
-                oasisHistory[day][record - 1].Insert(0,
-                    oasisHistory[day][record - 1].First() - oasisHistory[day][record].First()
-                );
-                record--;
-            } while (record > 0);
-        }
-
-        return oasisHistory.Sum(h => h.First().First());
-    }
-
-    public int RunPartOne(List<List<List<int>>> oasisHistory)
-    {
-
-        for (int day = 0; day < oasisHistory.Count; day++)
-        {
-            var record = 0;
-            do
-            {
-                oasisHistory[day].Add(new List<int>());
-                for (int entry = 1; entry < oasisHistory[day][record].Count; entry++)
-                {
-                    oasisHistory[day][record + 1].Add(
-                        oasisHistory[day][record][entry] - oasisHistory[day][record][entry - 1]);
-                }
-                record++;
-            } while (!oasisHistory[day][record].All(r => r == 0));
-
-            oasisHistory[day][record].Add(0);
-
-            do
-            {
-                oasisHistory[day][record - 1].Add(
-                    oasisHistory[day][record].Last() + oasisHistory[day][record - 1].Last()
-                );
-                record--;
-            } while (record > 0);
-        }
-
-        return oasisHistory.Sum(h => h.First().Last());
+            oasisHistory.Sum(h => h.First().Last()) :
+            oasisHistory.Sum(h => h.First().First());
     }
 
     public List<List<List<int>>> GetOasisHistory(TestType testType, Part part)
@@ -86,6 +24,39 @@ public class Day9 : AdventDay
                 .Select(int.Parse).ToList()
             ]);           
         }
+        
+        for (int day = 0; day < oasisHistory.Count; day++)
+        {
+            var record = 0;
+            do
+            {
+                oasisHistory[day].Add(new List<int>());
+                for (int entry = 1; entry < oasisHistory[day][record].Count; entry++)
+                {
+                    oasisHistory[day][record + 1].Add(
+                        oasisHistory[day][record][entry] - oasisHistory[day][record][entry - 1]);
+                }
+                record++;
+            } while (!oasisHistory[day][record].All(r => r == 0));
+        }
+
+        for (int day = 0; day < oasisHistory.Count; day++)
+        {
+            var record = oasisHistory[day].Count - 1;
+            oasisHistory[day][record].Insert(0, 0);
+
+            do
+            {
+                oasisHistory[day][record - 1].Insert(0,
+                    oasisHistory[day][record - 1].First() - oasisHistory[day][record].First()
+                );
+                oasisHistory[day][record - 1].Add(
+                    oasisHistory[day][record].Last() + oasisHistory[day][record - 1].Last()
+                );
+                record--;
+            } while (record > 0);
+        }
+
         return oasisHistory;
     }
 }
